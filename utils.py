4d7f63e4f26a803f8dd9d60ea95b1afe20e4a6f5
LuanFabricio <luanlimaleite@hotmail.com>
2024-11-23 21:35:52 -0300

    feat: Implementing triplet_loss function

--- utils.py
+++ utils.py
@@ -4,9 +4,21 @@ import numpy as np
 
 
 def cosine_distance(v1: np.ndarray, v2: np.ndarray) -> List[float]:
-    v1_v2 = np.dot(v1, v2)
-    v1_len = np.linalg.norm(v1)
-    v2_len = np.linalg.norm(v2)
+    v1 = v1 / np.linalg.norm(v1)
+    v2 = v2 / np.linalg.norm(v2)
 
-    cos_sim = v1_v2 / (v1_len * v2_len)
+    cos_sim = np.dot(v1, v2)
     return 1 - cos_sim, cos_sim
+
+
+def triplet_loss(
+        anchor: np.ndarray,
+        positive: np.ndarray,
+        negative: np.ndarray,
+        margin: float) -> float:
+
+    ap_dist, _ = cosine_distance(anchor, positive)
+    an_dist, _ = cosine_distance(anchor, negative)
+
+    loss = ap_dist - an_dist + margin
+    return max(loss, 0)
